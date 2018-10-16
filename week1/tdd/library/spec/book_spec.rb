2728d861728c9092a5ab74a508ce5baa41f8ae1c
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-10-16 19:57:40 +0100

    remove unwanted files and folders

--- week1/tdd/library/spec/book_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'book'
-
-describe Book do
-  it 'returns a title' do
-    book = Book.new('Great Expectations', 'Charles Dickens')
-  end
-end
--- week1/tdd/library/spec/library_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'library'
-
-describe Library do
-  it 'adds book and sees it in index' do
-    
-  end
-
-end
