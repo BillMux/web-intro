338c287875a40c954eae89cfcceff21a8d05e8b4
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-10-17 13:56:49 +0100

    capybara setup in spec_helper

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -8,13 +8,18 @@ get '/secret' do
   'this'
 end
 
+get '/cat-form' do
+  erb(:cat_form)
+end
+
 get '/random-cat' do
   @name = ['Amigo', 'Viking', 'Oscar'].sample
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
+  @last_name = params[:last_name]
   erb(:index)
 end
